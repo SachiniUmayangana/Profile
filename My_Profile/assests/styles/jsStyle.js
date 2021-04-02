35a954e8832f994b73920067bf0c28799139a184
SachiniUmayangana <sachi.umay@gmail.com>
2021-04-02 11:17:26 +0530

    Changed Done

--- My_Profile/assests/styles/jsStyle.js
+++ My_Profile/assests/styles/jsStyle.js
@@ -12,26 +12,39 @@ $('#wholeThing').on('click',function(){
     $('#star').val();
 });
 
-const cursor=document.getElementById('cursor');
-document.addEventListener('mousemove',(e)=>{
-    cursor.style.left=e.pageX+'px';
-    cursor.style.top=e.pageY+'px';
 
+const cursor=document.querySelector('.cursor');
+document.addEventListener('mousemove',(e)=>{
+        cursor.style.left=e.pageX+'px';
+        cursor.style.top=e.pageY+'px';
     }
-)
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
+);
+function toggleCheck() {
+    if(document.getElementById("myCheckbox").checked === true){
+        document.getElementById("wholeThing").style.background = "274659";
     }
-})
+    else {
+        document.getElementById("wholeThing").style.background = "#20202A";
+        document.getElementById("AboutMe").style.background = "#20202A";
+        document.getElementById("all").style.background = "#20202A";
+        document.getElementById("Skills").style.background = "#20202A";
+        document.getElementById("projects").style.background = "#20202A";
+        document.getElementById("pro").style.background = "#20202A";
+        document.getElementById("h4AboutMe").style.color = "#A0AAB3";
+        document.getElementById("SAchini").style.color = "#A0AAB3";
+        document.getElementById("leftSideDiv").style.background = "#20202A";
+        document.getElementById("leftSideDiv").style.border = "white";
+        document.getElementById("education").style.background = "#20202A";
+        document.getElementById("Assignments").style.background = "#20202A";
+        document.getElementById("text").style.background = "#20202A";
+        document.getElementById("text1").style.background = "#20202A";
+        document.getElementById("footer").style.background = "#20202A";
+        document.getElementById("ccc").style.background = "#20202A";
+        document.getElementById("row").style.background = "#20202A";
+    }
+}
+
+$('#pro2').on('mousemove',function (){
+    // alert('moving')
+    $('.style2').css('height','220px');
+});
